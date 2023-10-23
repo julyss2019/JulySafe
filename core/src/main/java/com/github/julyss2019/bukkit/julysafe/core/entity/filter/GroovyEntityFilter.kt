ec5cadc709324cbfd71b19fe88ae49f64b4e7987
void <july_ss_2017@outlook.com>
2023-10-23 17:27:00 +0800

    feat: 接入 VoidFramework Groovy

--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/entity/filter/GroovyEntityFilter.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/entity/filter/GroovyEntityFilter.kt
@@ -1,16 +1,21 @@
 package com.github.julyss2019.bukkit.julysafe.core.entity.filter
 
-import com.github.julyss2019.bukkit.julysafe.core.util.GroovyUtils
+import com.github.julyss2019.bukkit.julysafe.core.JulySafePlugin
 import com.github.julyss2019.bukkit.voidframework.yaml.Section
-import groovy.lang.Binding
+import com.void01.bukkit.voidframework.api.common.VoidFramework2
+import com.void01.bukkit.voidframework.api.common.groovy.GroovyBinding
+import com.void01.bukkit.voidframework.api.common.groovy.GroovyConfig
 import org.bukkit.entity.Entity
-import javax.script.SimpleBindings
 
 class GroovyEntityFilter : BaseEntityFilter() {
     private lateinit var script: String
 
     override fun filter(entity: Entity): Boolean {
-        return GroovyUtils.eval(script, Binding().apply { setVariable("entity", entity) }) as Boolean
+        return VoidFramework2.getGroovyManager().eval(script, GroovyConfig().apply {
+            this.parentClassLoader = JulySafePlugin::class.java.classLoader
+        }, GroovyBinding().apply {
+            setVariable("entity", entity)
+        }) as Boolean
     }
 
     override fun setProperties(section: Section) {
--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/item/filter/GroovyItemFilter.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/item/filter/GroovyItemFilter.kt
@@ -1,15 +1,22 @@
 package com.github.julyss2019.bukkit.julysafe.core.item.filter
 
-import com.github.julyss2019.bukkit.julysafe.core.util.GroovyUtils
+import com.github.julyss2019.bukkit.julysafe.core.JulySafePlugin
 import com.github.julyss2019.bukkit.voidframework.yaml.Section
-import groovy.lang.Binding
+import com.void01.bukkit.voidframework.api.common.VoidFramework2
+import com.void01.bukkit.voidframework.api.common.groovy.GroovyBinding
+import com.void01.bukkit.voidframework.api.common.groovy.GroovyConfig
 import org.bukkit.entity.Item
+
 
 class GroovyItemFilter : BaseItemFilter() {
     private lateinit var script: String
 
     override fun filter(item: Item): Boolean {
-        return GroovyUtils.eval(script, Binding().apply { setVariable("item", item) }) as Boolean
+        return VoidFramework2.getGroovyManager().eval(script, GroovyConfig().apply {
+            this.parentClassLoader = JulySafePlugin::class.java.classLoader
+        }, GroovyBinding().apply {
+            setVariable("item", item)
+        }) as Boolean
     }
 
     override fun setProperties(section: Section) {
