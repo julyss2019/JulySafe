16125266abc1b8d6401a7a6019735d8ed1c85a66
void <july_ss_2017@outlook.com>
2024-01-24 16:59:09 +0800

    feat: 将 Groovy 由 eval 切换为 class 以提高性能

--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/entity/filter/GroovyEntityFilter.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/entity/filter/GroovyEntityFilter.kt
@@ -4,25 +4,25 @@ import com.github.julyss2019.bukkit.julysafe.api.EntityFilter
 import com.github.julyss2019.bukkit.julysafe.core.JulySafePlugin
 import com.github.julyss2019.bukkit.voidframework.yaml.Section
 import com.void01.bukkit.voidframework.api.common.VoidFramework2
-import com.void01.bukkit.voidframework.api.common.groovy.GroovyBinding
 import com.void01.bukkit.voidframework.api.common.groovy.GroovyConfig
 import org.bukkit.entity.Entity
+import java.io.File
 
 class GroovyEntityFilter : BaseEntityFilter() {
     private lateinit var entityFilter: EntityFilter
     override fun filter(entity: Entity): Boolean {
-        return VoidFramework2.getGroovyManager().eval(script, GroovyConfig().apply {
-            this.parentClassLoader = JulySafePlugin::class.java.classLoader
-        }, GroovyBinding().apply {
-            setVariable("entity", entity)
-        }) as Boolean
+        return entityFilter.filter(entity)
     }
 
     override fun setProperties(section: Section) {
-        script = section.getString("script")
+        val scriptFile = File(JulySafePlugin.instance.dataFolder, section.getString("file"))
+
+        entityFilter = VoidFramework2.getGroovyManager().parseClass(scriptFile, GroovyConfig().apply {
+            sourceEncoding = "UTF-8"
+        }).newInstance() as EntityFilter
     }
 
     override fun toString(): String {
-        return "JavascriptEntityFilter(id=$id, script='$script')"
+        return "GroovyEntityFilter(entityFilter=$entityFilter)"
     }
 }
--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/item/filter/GroovyItemFilter.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/item/filter/GroovyItemFilter.kt
@@ -1,29 +1,29 @@
 package com.github.julyss2019.bukkit.julysafe.core.item.filter
 
+import com.github.julyss2019.bukkit.julysafe.api.ItemFilter
 import com.github.julyss2019.bukkit.julysafe.core.JulySafePlugin
 import com.github.julyss2019.bukkit.voidframework.yaml.Section
 import com.void01.bukkit.voidframework.api.common.VoidFramework2
-import com.void01.bukkit.voidframework.api.common.groovy.GroovyBinding
 import com.void01.bukkit.voidframework.api.common.groovy.GroovyConfig
 import org.bukkit.entity.Item
+import java.io.File
 
 
 class GroovyItemFilter : BaseItemFilter() {
-    private lateinit var script: String
-
+    private lateinit var itemFilter: ItemFilter
     override fun filter(item: Item): Boolean {
-        return VoidFramework2.getGroovyManager().eval(script, GroovyConfig().apply {
-            this.parentClassLoader = JulySafePlugin::class.java.classLoader
-        }, GroovyBinding().apply {
-            setVariable("item", item)
-        }) as Boolean
+        return itemFilter.filter(item)
     }
 
     override fun setProperties(section: Section) {
-        script = section.getString("script")
+        val scriptFile = File(JulySafePlugin.instance.dataFolder, section.getString("file"))
+
+        itemFilter = VoidFramework2.getGroovyManager().parseClass(scriptFile, GroovyConfig().apply {
+            sourceEncoding = "UTF-8"
+        }).newInstance() as ItemFilter
     }
 
     override fun toString(): String {
-        return "EnumItemFilter(id = $id, script = $script)"
+        return "GroovyEntityFilter(itemFilter=$itemFilter)"
     }
 }
