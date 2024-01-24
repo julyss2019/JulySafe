d770f7c8c665e357d2241728a73d02c27dcbab4a
void <july_ss_2017@outlook.com>
2024-01-24 14:59:04 +0800

    feat: 为 BossBar 通知增加动态进度条

--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/executor/notification/BossBarNotification.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/executor/notification/BossBarNotification.kt
@@ -10,9 +10,14 @@ import org.bukkit.boss.BarStyle
 import org.bukkit.boss.BossBar
 
 class BossBarNotification : Notification {
+    enum class ProgressType {
+        INCREASING, DECREASING
+    }
+
     private lateinit var title: String
     private lateinit var color: BarColor
     private lateinit var style: BarStyle
+    private lateinit var progressType: ProgressType
     private var bossBar: BossBar? = null
     override fun notifyCountdown(messageProcessor: MessageProcessor, currentCountdown: Int, maxCountdown: Int) {
         if (bossBar == null) {
@@ -20,17 +25,23 @@ class BossBarNotification : Notification {
             JulySafePlugin.instance.bossBarManager.registerBossBar(bossBar!!)
         }
 
-        bossBar!!.title = messageProcessor.process(title)
+        bossBar!!.run {
+            title = messageProcessor.process(this@BossBarNotification.title)
+            val tmp = currentCountdown / maxCountdown.toDouble()
+            progress = if (progressType == ProgressType.DECREASING) tmp else 1 - tmp
+        }
     }
 
     override fun notifyCompleted(messageProcessor: MessageProcessor) {
-        throw UnsupportedOperationException("BoosBar is not allowed to notify completed")
+        throw UnsupportedOperationException()
     }
 
     override fun setProperties(section: Section) {
         title = section.getString("title")
         color = section.getEnum("color", BarColor::class.java, DefaultValue.of(BarColor.RED))
         style = section.getEnum("style", BarStyle::class.java, DefaultValue.of(BarStyle.SOLID))
+        progressType =
+            section.getEnum("progress_type", ProgressType::class.java, DefaultValue.of(ProgressType.DECREASING))
     }
 
     override fun clear() {
