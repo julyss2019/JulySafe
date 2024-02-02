6403dba84d71a32c7cd87f5c6634b983b1536df3
void <july_ss_2017@outlook.com>
2024-02-02 17:38:33 +0800

    feat: 将爆炸掉落物百分比设为 0

--- core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/listener/EntityExplodeLimitListener.kt
+++ core/src/main/java/com/github/julyss2019/bukkit/julysafe/core/listener/EntityExplodeLimitListener.kt
@@ -1,11 +1,9 @@
 package com.github.julyss2019.bukkit.julysafe.core.listener
 
 import com.github.julyss2019.bukkit.julysafe.core.kotlin.extension.getAsSimpleString
-import com.github.julyss2019.bukkit.julysafe.core.module.BlockExplodeLimitModule
 import com.github.julyss2019.bukkit.julysafe.core.module.EntityExplodeLimitModule
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockExplodeEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 
 class EntityExplodeLimitListener(private val module: EntityExplodeLimitModule) : Listener {
@@ -13,11 +11,10 @@ class EntityExplodeLimitListener(private val module: EntityExplodeLimitModule) :
     fun onEntityExplodeEvent(event: EntityExplodeEvent) {
         val entity = event.entity
 
-        if (!module.worldSet.contains(entity.world)) {
-            return
+        if (module.worldSet.contains(entity.world)) {
+            event.isCancelled = true
+            event.yield = 0F
+            module.debug("cancelled, entity = ${entity.getAsSimpleString()}.")
         }
-
-        event.isCancelled = true
-        module.debug("cancelled, entity = ${entity.getAsSimpleString()}.")
     }
 }
